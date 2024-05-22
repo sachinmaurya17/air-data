6bade366dc325b2f1880a629bce7331aff0e896b
sachinmaurya17 <sachin655599@gmail.com>
2024-05-22 14:59:51 +0530

    Subject: setup and initialize the project  Messgae: setup and initialize the project

---
+++ src/main/java/com/example/airodata/AiroDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.airodata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiroDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiroDataApplication.class, args);
+    }
+
+}
